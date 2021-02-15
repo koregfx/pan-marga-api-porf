8168d5e8b2d46f607766af62f8ce7c4bce3eee12
kore <koregfx@gmail.com>
2021-02-15 13:39:56 +0100

    fixing isues

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/', (req,res)=>{
     res.json('hola mundo')
 })
 
-mongoose.connect(process.env.URLDB, {useNewUrlParser: true , useUnifiedTopology: true }, (err, res) => {
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true , useUnifiedTopology: true }, (err, res) => {
 
     if (err) throw err;
 
